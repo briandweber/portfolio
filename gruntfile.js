63c9fe1bdcfc13092438eacb74bb3c6c81fafdc5
Brian Weber <briandweber@gmail.com>
2017-03-20 22:22:03 -0500

    added resized images

--- gruntfile.js
+++ gruntfile.js
@@ -7,19 +7,28 @@ module.exports = function(grunt){
         options: {
           engine: 'im',
           sizes: [{
-            width: 450,
+            width: 300,
+            height: 300,
             suffix: '_small',
             quality: 30
           }, {
-            width: 750,
+            width: 450,
+            height: 450,
             suffix: '_medium',
             quality: 30
           }, {
+            width: 600,
+            height: 600,
+            suffix: '_med-large',
+            quality: 30
+          }, {
             width: 800,
+            height: 800,
             suffix: '_large_1x',
             quality: 30
           }, {
             width: 1600,
+            height: 1600,
             suffix: '_large_2x',
             quality: 30
           }]
